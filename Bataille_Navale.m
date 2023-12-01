99830b7cdf81048996dcdf272879ad186be38545
Hisitis <116912376+Hisitis@users.noreply.github.com>
2023-12-01 16:31:17 +0100

    Bataille_Navale.m

--- Bataille_Navale.m
+++ Bataille_Navale.m
@@ -1,112 +1,184 @@
-%projet bataille navale
+%test Bataille navale
 
 clc
 clear
 close all
 
-%n = input('Quelle taille de plateau souhaitez-vous ? : ')
-n = 10;
+n=10;
 
-MerIA = zeros(n)
+merIA=zeros(n);
 
+t=0;
+c=0;
 
-%generation du premier bateau
-
-x = randi([1,n]);
-y = randi([1,n]);
-
-MerIA(x,y)  = 1
-
-%S = randi([1,2])
-S = 1;
-
-if S  ==  1 %vertical
-
-  if x  <= n-5
-
-    for i=0:4
-
-      MerIA(x+i,y)  = 1;
-
-      if y > 1
-
-              MerIA(x+i,y-1)  = -1;
-
-      end
-      MerIA(x+i,y+1)  = -1;
+while t < 4
 
 
-    end
-     MerIA(x+i+1,y)  = -1;
 
-    if x  > 1
-            MerIA(x-1,y)  = -1;
+    if t==0
+        b=4;
+        t=t+1;
+    elseif t==1
+        b=3;
+        t=t+1;
+    elseif t==2
+        b=2;
+        c=c+1;
+        if c==2
+            t=t+1;
+        end
+    elseif t==3
+        b=1;
+        t=t+1;
     end
 
-  elseif x >= n-5
+S=randi([0,1]);
+Coo=[];
+a=1;
 
-    for i=0:4
+if S==0
 
-      MerIA(x-i,y)  = 1;
+    for i=1:n
+        for j=1:n
+       
+            if merIA(i,j)==0
+            
+                Coo(a,1)=i;
+                Coo(a,2)=j;
+                a=a+1;
 
-      if y > 1
-
-              MerIA(x-i,y-1)  = -1;
-
-      end
-      MerIA(x-i,y+1)  = -1;
-
-
+            end
+        
+        end
     end
-     MerIA(x-i-1,y)  = -1;
 
-    if x  < n
-            MerIA(x+1,y)  = -1;
+    q=true;
+    while q
+
+        l=randi([1,length(Coo)-b]);
+        d=0;
+
+        if Coo(l,1)==Coo(l+1,1) && Coo(l,2)+1==Coo(l+1,2)
+            for i=0:b-1
+            
+                if Coo(l+i,1)==Coo(l+i+1,1) && Coo(l+i,2)+1==Coo(l+i+1,2)
+                    d=d+1;
+                end
+        
+            end
+        end
+        if d==b
+            q=false;
+        end
     end
 
-  end
+    for i=0:b
+        merIA(Coo(l+i,1),Coo(l+i,2))=1;
+        
+        if Coo(l,1) > 1
+        
+           merIA(Coo(l+i,1)-1,Coo(l+i,2))=-1; 
+
+        end
+        if Coo(l,1) < n
+        
+           merIA(Coo(l+i,1)+1,Coo(l+i,2))=-1; 
+
+        end
+        if l-1 >= 1
+            if Coo(l-1,1) >= 1 && Coo(l-1,1) == Coo(l,1)
+              
+               merIA(Coo(l-1,1),Coo(l-1,2))=-1; 
+    
+            end
+        end
+        if l+b+1 <= length(Coo)
+            if Coo(l+b+1,1) <= n && Coo(l+b+1,1) == Coo(l+b,1)  
+    
+               merIA(Coo(l+b+1,1),Coo(l+b+1,2))=-1; 
+    
+            end
+        end
+    end
+
+elseif S==1
+
+    for i=1:n
+        for j=1:n
+       
+            if merIA(j,i)==0
+            
+                Coo(a,1)=j;
+                Coo(a,2)=i;
+                a=a+1;
+
+            end
+        
+        end
+    end
+
+    q=true;
+    while q
+
+        l=randi([1,length(Coo)-b]);
+        d=0;
+
+        if Coo(l,2)==Coo(l+1,2) && Coo(l,1)+1==Coo(l+1,1)
+            for i=0:b-1
+            
+                if Coo(l+i,2)==Coo(l+i+1,2) && Coo(l+i,1)+1==Coo(l+i+1,1)
+                    d=d+1;
+                end
+        
+            end
+        end
+        if d==b
+            q=false;
+        end
+    end
+
+    for i=0:b
+        merIA(Coo(l+i,1),Coo(l+i,2))=1;
+        
+        if Coo(l,2) > 1
+        
+           merIA(Coo(l+i,1),Coo(l+i,2)-1)=-1; 
+
+        end
+        if Coo(l,2) < n
+        
+           merIA(Coo(l+i,1),Coo(l+i,2)+1)=-1; 
+
+        end
+        if l-1 >= 1
+            if Coo(l-1,2) >= 1 && Coo(l-1,2) == Coo(l,2)
+              
+               merIA(Coo(l-1,1),Coo(l-1,2))=-1; 
+    
+            end
+        end
+        if l+b+1 <= length(Coo)
+            if Coo(l+b+1,2) <= n && Coo(l+b+1,2) == Coo(l+b,2)  
+    
+               merIA(Coo(l+b+1,1),Coo(l+b+1,2))=-1; 
+    
+            end
+        end
+    end
+end
+
 
 end
 
-  k=1;
-  z=1;
-
-  for i=1:n
+for i=1:n
     for j=1:n
-
-      if MerIA(i,j)==0
-        Coo1(k,1)=i;
-        Coo1(k,2)=j;
-        k=k+1;
-      endif
-      if MerIA(j,i)==0
-        Coo2(z,1)=i;
-        Coo2(z,2)=j;
-        z=z+1;
-      endif
-
-    endfor
-  endfor
-
-l = randi([1,length(Coo1)]);
-
-x = Coo1(l,2)
-y = Coo1(l,1)
-
-%Coo1 horizontal
-
-
-%S = randi([1,2])
-S = 2;
-
-if S  ==  2 %horizontal
-
-  for i=0:3
-
-    if
-
-  endfor
-
+        if merIA(i,j)==-1
+            merIA(i,j)=0;
+        end
+    end
 end
 
-imagesc(MerIA)
+% Visualize the grid
+%imagesc(merIA)
+imshow(merIA, 'InitialMagnification', 'fit');
+title('Distribution of Ships');
